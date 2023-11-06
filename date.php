6692894070e4310a02e5a5fb06cae254b5878e29
riggsico <132190687+riggsico@users.noreply.github.com>
2023-11-07 00:12:04 +0800

    date.php

---
+++ date.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="process.php">
+
+    <input type="date" id="dateInput" name="date">
+    <br>
+    <input type="radio" value="10-10:45" name="time" >
+    <label for="">10-10:45</label>
+    <input type="radio" value="11-11:45" name="time">
+    <label for="">11-11:45</label>
+    <input type="radio" value="1-1:45" name="time">
+    <label for="">1-1:45</label>
+    <input type="radio" value="2-2:45" name="time">
+    <label for="">2-2:45</label>
+    <input type="radio" value="3-3:45" name="time">
+    <label for="">3-3:45</label>
+    <br>
+    <input type="submit">
+    </form>
+    <script src="date.js"></script>
+</body>
+</html>
